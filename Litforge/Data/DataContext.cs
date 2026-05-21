a6df1cadb4013b4432dca4e6bc859dbb2c5b62e6
Saurav Pant <sauravpant777@gmail.com>
2026-05-21 10:09:41 +0545

    Add DataContext for database configuration

---
+++ Litforge/Data/DataContext.cs
@@ -0,0 +1,37 @@
+﻿using Litforge.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Litforge.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        public DbSet<BookAuthor> BookAuthors { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<BookCategory> BookCategories { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        public DbSet<Reviewer> Reviewers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BookAuthor>().HasKey(c => new { c.BookId, c.AuthorId });
+            modelBuilder.Entity<BookCategory>().HasKey(c => new { c.BookId, c.CategoryId });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
